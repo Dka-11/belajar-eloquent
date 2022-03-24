8e48bec87dffede0bf594b4cec13e6bb06d1b101
Dka-11 <adikaahmad05@gmail.com>
2022-03-24 09:51:39 +0700

    Memberi function

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -4,7 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Pegawai;
+
 class PegawaiController extends Controller
 {
-    //
+    public function index(){
+        $pegawai = Pegawai::all();
+
+        return view('pegawai',[
+            'pegawai' => $pegawai
+        ]);
+    }
 }
